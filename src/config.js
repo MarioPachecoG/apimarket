7aba993df020e60e7e707b8cf661a4099234ff81
MarioPachecoG <mapg0324@gmail.com>
2025-04-28 16:43:31 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import{config} from 'dotenv'
 config()
 
 export const PORT= process.env.PORT || 3000
-export const DB_HOST=process.env.DB_HOST || '${{MySQL-ZcT5.MYSQLHOST}}'
-export const DB_PORT=process.env.DB_PORT || '${{MySQL-ZcT5.MYSQLPORT}}'
-export const DB_USER=process.env.DB_USER || '${{MySQL-ZcT5.MYSQLUSER}}'
-export const DB_PASSWORD=process.env.DB_PASSWORD || '${{MySQL-ZcT5.MYSQLPASSWORD}}'
-export const DB_DATABASE=process.env.DB_DATABASE || '${{MySQL-ZcT5.MYSQLDATABASE}}'
+export const DB_HOST=process.env.DB_HOST || 'mysql-zct5.railway.internal'
+export const DB_PORT=process.env.DB_PORT || '3306'
+export const DB_USER=process.env.DB_USER || 'root'
+export const DB_PASSWORD=process.env.DB_PASSWORD || 'pbHnGwoKeHKYjoRlVjoHcOraVZYGbLHb'
+export const DB_DATABASE=process.env.DB_DATABASE || 'railway'
