bd309f36cc84ae5d83b6a81df6c9fc3bc4c69fd4
MarioPachecoG <mapg0324@gmail.com>
2025-04-28 16:38:32 -0600

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ import{config} from 'dotenv'
 config()
 
 export const PORT= process.env.PORT || 3000
-export const DB_HOST=process.env.DB_HOST || '${{MySQL-ZcT5.MYSQLHOST}}
+export const DB_HOST=process.env.DB_HOST || '${{MySQL-ZcT5.MYSQLHOST}}'
 export const DB_PORT=process.env.DB_PORT || '${{MySQL-ZcT5.MYSQLPORT}}'
 export const DB_USER=process.env.DB_USER || '${{MySQL-ZcT5.MYSQLUSER}}'
 export const DB_PASSWORD=process.env.DB_PASSWORD || '${{MySQL-ZcT5.MYSQLPASSWORD}}'
